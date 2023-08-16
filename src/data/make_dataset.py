c497a17200322b8640a6102c788026f4b99e5f15
LouiseDurandJanin <louisedurandjanin@gmail.com>
2023-08-16 14:27:03 +0200

    notebook modified

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,20 +1,121 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import pandas as pd
+import numpy as np
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
-@click.argument(("data/raw/usagers-2021.csv", "data/raw/caracteristiques-2021.csv",
-                 "data/raw/lieux-2021.csv", "data/raw/vehicules-2021.csv"), nargs = 4, type=click.Path(exists=True))
-@click.argument('data\raw\preprocessed', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('input_filepath_users', type=click.Path(exists=False), required=0)
+@click.argument('input_filepath_caract', type=click.Path(exists=False), required=0)
+@click.argument('input_filepath_places', type=click.Path(exists=False), required=0)
+@click.argument('input_filepath_veh', type=click.Path(exists=False), required=0)
+@click.argument('output_filepath', type=click.Path(exists=False), required=0)
+def main(input_filepath_users, input_filepath_caract, input_filepath_places, input_filepath_veh, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+        cleaned data ready to be analyzed (saved in ../preprocessed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    # Prompt the user for input file paths
+    input_filepath_users = click.prompt('Enter the file path for users data (e.g., data/users.csv)', type=click.Path(exists=True))
+    input_filepath_caract = click.prompt('Enter the file path for caract data (e.g., data/caract.csv)', type=click.Path(exists=True))
+    input_filepath_places = click.prompt('Enter the file path for places data (e.g., data/places.csv)', type=click.Path(exists=True))
+    input_filepath_veh = click.prompt('Enter the file path for veh data (e.g., data/veh.csv)', type=click.Path(exists=True))
+    output_filepath = click.prompt('Enter the file path for the output preprocessed data (e.g., output/preprocessed_data.csv)', type=click.Path())
+    
+    # Call the main data processing function with the provided file paths
+    process_data(input_filepath_users, input_filepath_caract, input_filepath_places, input_filepath_veh, output_filepath)
+
+def process_data(input_filepath_users, input_filepath_caract, input_filepath_places, input_filepath_veh, output_filepath):
+ 
+    #--Importing dataset
+    df_users = pd.read_csv(input_filepath_users, sep=";")
+    df_caract = pd.read_csv(input_filepath_caract, sep=";", header=0, low_memory=False)
+    df_places = pd.read_csv(input_filepath_places, sep = ";", encoding='utf-8')
+    df_veh = pd.read_csv(input_filepath_veh, sep=";")
+
+
+        #--Creating new columns
+    nb_victim = pd.crosstab(df_users.Num_Acc, "count").reset_index()
+    nb_vehicules = pd.crosstab(df_veh.Num_Acc, "count").reset_index()
+    df_users["year_acc"] = df_users["Num_Acc"].astype(str).apply(lambda x : x[:4]).astype(int)
+    df_users["victim_age"] = df_users["year_acc"]-df_users["an_nais"]
+    for i in df_users["victim_age"] :
+        if (i>120)|(i<0):
+            df_users["victim_age"].replace(i,np.nan)
+
+    df_users.drop(["year_acc","an_nais"], inplace=True, axis=1)
+    df_caract["hour"] = df_caract["hrmn"].astype(str).apply(lambda x : x[:-3])
+    df_caract.drop(['hrmn'], inplace=True, axis=1)
+
+    #--Replacing names 
+    df_users.grav.replace([1,2,3,4], [1,3,4,2], inplace = True)
+    df_caract.rename({"agg" : "agg_"},  inplace = True, axis = 1)
+    corse_replace = {"2A":"201", "2B":"202"}
+    df_caract["dep"] = df_caract["dep"].str.replace("2A", "201")
+    df_caract["dep"] = df_caract["dep"].str.replace("2B", "202")
+    df_caract["com"] = df_caract["com"].str.replace("2A", "201")
+    df_caract["com"] = df_caract["com"].str.replace("2B", "202")
+
+    #--Converting columns types
+    df_caract[["dep","com", "hour"]] = df_caract[["dep","com", "hour"]].astype(int)
+
+    dico_to_float = { 'lat': float, 'long':float}
+    df_caract["lat"] = df_caract["lat"].str.replace(',', '.')
+    df_caract["long"] = df_caract["long"].str.replace(',', '.')
+    df_caract = df_caract.astype(dico_to_float)
+
+
+    #--Grouping the modalities 
+    dico = {1:0, 2:1, 3:1, 4:1, 5:1, 6:1,7:1, 8:0, 9:0}
+    df_caract["atm"] = df_caract["atm"].replace(dico)
+    catv_value = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,30,31,32,33,34,35,36,37,38,39,40,41,42,43,50,60,80,99]
+    catv_value_new = [0,1,1,2,1,1,6,2,5,5,5,5,5,4,4,4,4,4,3,3,4,4,1,1,1,1,1,6,6,3,3,3,3,1,1,1,1,1,0,0]
+    df_veh['catv'].replace(catv_value, catv_value_new, inplace = True)
+
+    #--Merging datasets 
+    fusion1= df_users.merge(df_veh, on = ["Num_Acc","num_veh", "id_vehicule"], how="inner")
+    fusion1 = fusion1.sort_values(by = "grav", ascending = False)
+    fusion1 = fusion1.drop_duplicates(subset = ['Num_Acc'], keep="first")
+    fusion2 = fusion1.merge(df_places, on = "Num_Acc", how = "left")
+    df = fusion2.merge(df_caract, on = 'Num_Acc', how="left")
+
+    #--Adding new columns
+    df = df.merge(nb_victim, on = "Num_Acc", how = "inner")
+    df.rename({"count" :"nb_victim"},axis = 1, inplace = True) 
+    df = df.merge(nb_vehicules, on = "Num_Acc", how = "inner") 
+    df.rename({"count" :"nb_vehicules"},axis = 1, inplace = True)
+
+    #--Modification of the target variable  : 1 : prioritary // 0 : non-prioritary
+    df['grav'].replace([2,3,4], [0,1,1], inplace=True)
+
+
+    #--Replacing values -1 and 0 
+    col_to_replace0_na = [ "trajet", "catv", "motor"]
+    col_to_replace1_na = [ "trajet", "secu1", "catv", "obsm", "motor", "circ", "surf", "situ", "vma", "atm", "col"]
+    df[col_to_replace1_na] = df[col_to_replace1_na].replace(-1, np.nan)
+    df[col_to_replace0_na] = df[col_to_replace0_na].replace(0, np.nan)
+
+
+    #--Dropping columns 
+    list_to_drop = ['senc','larrout','actp', 'manv', 'choc', 'nbv', 'prof', 'plan', 'Num_Acc', 'id_vehicule', 'num_veh', 'pr', 'pr1','voie', 'trajet',"secu2", "secu3",'adr', 'v1', 'lartpc','occutc','v2','vosp','locp','etatp', 'infra', 'obs' ]
+    df.drop(list_to_drop, axis=1, inplace=True)
+    missing_values = df.isna().sum()
+
+    missing_values_sorted = missing_values.sort_values(ascending=False)
+
+    #--Filling NaN values
+    col_to_fill_na = ["surf", "situ", "circ", "col", "motor"]
+    df[col_to_fill_na] = df[col_to_fill_na].fillna(df[col_to_fill_na].mode().iloc[0])
+
+    final_preprocessed_data = df.dropna(axis=0)
+
+    # Save the final preprocessed data to the output_filepath
+    final_preprocessed_data.to_csv(output_filepath, index=False)
 
 
 if __name__ == '__main__':
