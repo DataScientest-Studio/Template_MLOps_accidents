8969aca2c65ea6eccfbeb11d84dadf9eed598b48
LouiseDurandJanin <louisedurandjanin@gmail.com>
2023-08-21 15:22:45 +0200

    delete dotenv in make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 import click
 import logging
 from sklearn.model_selection import train_test_split
@@ -139,8 +138,5 @@ if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
