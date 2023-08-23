aa0fd0961fe7c6ae9d2f142f2bcd1de1c98cfe90
LouiseDurandJanin <louisedurandjanin@gmail.com>
2023-08-23 13:31:42 +0200

    included json file option in argument

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,35 +1,35 @@
 
-import click
 import joblib
 import pandas as pd
+import sys
+import json
 
 # Load your saved model
 loaded_model = joblib.load("src/models/trained_model.joblib")
 
-@click.command()
-def main():
-    # Load your training dataset (replace 'train_data.csv' with your file)
-    X_train = pd.read_csv("data/preprocessed/X_train.csv")
-
-    # Get feature names from X_train columns
-    feature_names = X_train.columns.tolist()
-
-    features = {}
-
-    # Get user input for each feature
-    for feature_name in feature_names:
-        feature_value = click.prompt(f"Enter value for {feature_name}", type=float)
-        features[feature_name] = feature_value
-
-    # Predict using the model
-    result = predict_model(features)
-    print("Prediction:", result)
 
 def predict_model(features):
     input_df = pd.DataFrame([features])
+    print(input_df)
     prediction = loaded_model.predict(input_df)
     return prediction
 
-if __name__ == "__main__":
-    main()
+def get_feature_values_manually(feature_names):
+    features = {}
+    for feature_name in feature_names:
+        feature_value = float(input(f"Enter value for {feature_name}: "))
+        features[feature_name] = feature_value
+    return features
 
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        json_file = sys.argv[1]
+        with open(json_file, 'r') as file:
+            features = json.load(file)
+    else:
+        X_train = pd.read_csv("data/preprocessed/X_train.csv")
+        feature_names = X_train.columns.tolist()
+        features = get_feature_values_manually(feature_names)
+
+    result = predict_model(features)
+    print(result)
