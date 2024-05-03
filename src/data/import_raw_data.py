a75051e39536daba1e306a93f0d0573f597b8e85
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2024-05-03 10:02:30 +0200

    Update import_raw_data.py
    remove references to check existing files and check existing folder

--- src/data/import_raw_data.py
+++ src/data/import_raw_data.py
@@ -1,42 +1,35 @@
 import requests
 import os
 import logging
-from check_structure import check_existing_file, check_existing_folder
 
 
 def import_raw_data(raw_data_relative_path,
                     filenames,
                     bucket_folder_url):
     '''import filenames from bucket_folder_url in raw_data_relative_path'''
-    if check_existing_folder(raw_data_relative_path):
-        os.makedirs(raw_data_relative_path)
+
     # download all the files
     for filename in filenames:
         input_file = os.path.join(bucket_folder_url, filename)
         output_file = os.path.join(raw_data_relative_path, filename)
-        if check_existing_file(output_file):
-            object_url = input_file
-            print(f'downloading {input_file} \
-                  as {os.path.basename(output_file)}')
-            response = requests.get(object_url)
-            if response.status_code == 200:
-                # Process the response content as needed
-                content = response.text
-                text_file = open(output_file, "wb")
-                text_file.write(content.encode('utf-8'))
-                text_file.close()
-            else:
-                print(f'Error accessing the object {input_file}:',
-                      response.status_code)
+        object_url = input_file
+        print(f'downloading {input_file} as {os.path.basename(output_file)}')
+        response = requests.get(object_url)
+        if response.status_code == 200:
+            # Process the response content as needed
+            content = response.text
+            text_file = open(output_file, "wb")
+            text_file.write(content.encode('utf-8'))
+            text_file.close()
+        else:
+            print(f'Error accessing the object {input_file}:',
+                  response.status_code)
 
 
 def main(raw_data_relative_path="./data/raw",
-         filenames=["caracteristiques-2021.csv",
-                    "lieux-2021.csv",
-                    "usagers-2021.csv",
-                    "vehicules-2021.csv"],
-         bucket_folder_url="https://mlops-project-db.s3.\
-                            eu-west-1.amazonaws.com/accidents/"
+         filenames=["caracteristiques-2021.csv", "lieux-2021.csv",
+                    "usagers-2021.csv", "vehicules-2021.csv"],
+         bucket_folder_url="https://mlops-project-db.s3.eu-west-1.amazonaws.com/accidents/"
          ):
     """ Upload data from AWS s3 in ./data/raw
     """
