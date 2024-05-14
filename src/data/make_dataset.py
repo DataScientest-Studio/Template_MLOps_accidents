1f2665cef68d3797d7d3cf27f185b78e57c8da93
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2024-05-14 10:46:47 +0200

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -152,5 +152,5 @@ if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
-    main(input_filepath="../../data/interim",
-         output_filepath="../../data/preprocessed")
+    main(input_filepath="data/interim",
+         output_filepath="data/preprocessed")
