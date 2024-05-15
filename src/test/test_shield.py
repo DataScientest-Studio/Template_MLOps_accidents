e448415c3b061a9073a64d5e16e4d9531be2663e
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2024-05-15 13:19:36 +0200

    fix(test_shield.py): EP 8bis
    -fix `post` method; changed into `get`

--- src/test/test_shield.py
+++ src/test/test_shield.py
@@ -117,12 +117,11 @@ def test_label_prediction():
 # ---------- EP8bis: /label_pred_test -------------------------------------------
 def test_label_pred_test():
     time.sleep(delay)
-    response = requests.post(url="http://api:8000/label_pred_test",
+    response = requests.get(url="http://api:8000/label_pred_test",
                              headers=header_admin)
     assert response.status_code == 200
     message = "Test EP2: /add_user: PASSED"
     print(message)
-
 
 
 # ---------- EP9: /update_f1_score --------------------------------------------
