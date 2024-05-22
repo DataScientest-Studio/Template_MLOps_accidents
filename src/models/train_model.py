692b3f4b5654341bd7e4e561d8631e22548aed48
raphrb2 <raphael.be@datascientest.com>
2024-05-22 17:44:13 +0200

    added files for template bentoml

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,11 +1,12 @@
-
 import sklearn
 import pandas as pd 
 from sklearn import ensemble
 import joblib
 import numpy as np
+import bentoml
+from bentoml.io import NumpyNdarray
 
-print(joblib.__version__)
+#print(joblib.__version__)
 
 X_train = pd.read_csv('data/preprocessed/X_train.csv')
 X_test = pd.read_csv('data/preprocessed/X_test.csv')
@@ -19,7 +20,23 @@ rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1)
 #--Train the model
 rf_classifier.fit(X_train, y_train)
 
-#--Save the trained model to a file
-model_filename = './src/models/trained_model.joblib'
-joblib.dump(rf_classifier, model_filename)
-print("Model trained and saved successfully.")
+#--Test the model
+rf_classifier.predict(X_test)
+
+#--Get the model accuracy
+accuracy = rf_classifier.score(X_test, y_test)
+
+print(f"Model accuracy: {accuracy}")
+
+# test the model on a single observation
+test_data = X_test.iloc[0]
+# print the actual label
+print(f"Actual label: {y_test[0]}")
+# print the predicted label
+print(f"Predicted label: {rf_classifier.predict([test_data])[0]}")
+
+
+# #--Save the trained model to a file
+# model_filename = './src/models/trained_model.joblib'
+# joblib.dump(rf_classifier, model_filename)
+# print("Model trained and saved successfully.")
