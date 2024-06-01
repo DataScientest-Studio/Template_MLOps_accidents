e55d3e33128ab4a2fe5fbd8d17f0915e767fc34d
TDLS-13 <tradingdlsoftware@gmail.com>
2024-06-01 18:43:39 +0200

    Essai d'un modèle de regression linéaire à la place d'un random forest. Les hyper-paramètres tolérance et max itérations peuvent être transmis en ligne de commande pour automatisation. Changement des paths preprocessed en processed pour être conforme à l'organisation du dépot.

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,25 +1,38 @@
-
+import sys
 import sklearn
 import pandas as pd 
-from sklearn import ensemble
+#from sklearn import ensemble
 import joblib
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
-print(joblib.__version__)
 
-X_train = pd.read_csv('data/preprocessed/X_train.csv')
-X_test = pd.read_csv('data/preprocessed/X_test.csv')
-y_train = pd.read_csv('data/preprocessed/y_train.csv')
-y_test = pd.read_csv('data/preprocessed/y_test.csv')
-y_train = np.ravel(y_train)
-y_test = np.ravel(y_test)
-
-rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1)
-
-#--Train the model
-rf_classifier.fit(X_train, y_train)
-
-#--Save the trained model to a file
-model_filename = './src/models/trained_model.joblib'
-joblib.dump(rf_classifier, model_filename)
-print("Model trained and saved successfully.")
+if __name__ == "__main__":
+    s_tol = float(sys.argv[1])          # 0.001
+    s_max_iter = int(sys.argv[2])       # 100
+    
+    print(joblib.__version__)
+    
+    
+    X_train = pd.read_csv('data/processed/X_train.csv')
+    X_test = pd.read_csv('data/processed/X_test.csv')
+    y_train = pd.read_csv('data/processed/y_train.csv')
+    y_test = pd.read_csv('data/processed/y_test.csv')
+    
+    
+    y_train = np.ravel(y_train)
+    y_test = np.ravel(y_test)
+    
+    #rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1)
+    log_reg = LogisticRegression(tol=s_tol, solver='liblinear', max_iter=s_max_iter, verbose=1, random_state=3)
+    
+    #--Train the model
+    #rf_classifier.fit(X_train, y_train)
+    log_reg.fit(X_train, y_train)
+    
+    #--Save the trained model to a file
+    model_filename = './src/models/log_reg_trained_model.joblib'
+    #joblib.dump(rf_classifier, model_filename)
+    joblib.dump(log_reg, model_filename)
+    
+    print("Model trained and saved successfully.")
