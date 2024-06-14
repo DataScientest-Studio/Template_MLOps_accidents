f1bf761efb7eab2a9c2d9ffc18990f07b83ea814
DocJoe <josef.hartmann@hsup.de>
2024-06-14 12:18:28 +0200

    Update files for local file system and path references

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,17 +1,18 @@
-
 import joblib
 import pandas as pd
 import sys
 import json
 
 # Load your saved model
-loaded_model = joblib.load("./src/models/trained_model.joblib")
+loaded_model = joblib.load("../../models/trained_model.joblib")
+
 
 def predict_model(features):
     input_df = pd.DataFrame([features])
     print(input_df)
     prediction = loaded_model.predict(input_df)
     return prediction
+
 
 def get_feature_values_manually(feature_names):
     features = {}
@@ -20,13 +21,14 @@ def get_feature_values_manually(feature_names):
         features[feature_name] = feature_value
     return features
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         json_file = sys.argv[1]
-        with open(json_file, 'r') as file:
+        with open(json_file, "r") as file:
             features = json.load(file)
     else:
-        X_train = pd.read_csv("data/preprocessed/X_train.csv")
+        X_train = pd.read_csv("../data/preprocessed/X_train.csv")
         feature_names = X_train.columns.tolist()
         features = get_feature_values_manually(feature_names)
 
