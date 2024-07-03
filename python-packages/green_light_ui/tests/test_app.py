835584f10f1b42d8a48c2e89aaac47110ed24380
Evangelos Stromatias <evangelos.stromatias@gmail.com>
2024-07-03 13:45:27 +0200

    fixed test streamlit app path

--- python-packages/green_light_ui/tests/test_app.py
+++ python-packages/green_light_ui/tests/test_app.py
@@ -1,7 +1,10 @@
+from pathlib import Path
 from streamlit.testing.v1 import AppTest
 
 
 def test_the_streamlit_app_runs():
     """Tests that the streamlit app starts properly."""
-    at = AppTest.from_file("src/green_light_ui/app.py").run()
+    current_path = Path(__file__).parent
+    project_path = current_path.parent
+    at = AppTest.from_file(str(project_path / "src" / "green_light_ui" / "app.py")).run()
     assert True
