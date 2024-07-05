23480ed34e3905ff827f02d9a1d025933b59a4ef
DocJoe <josef.hartmann@hsup.de>
2024-07-05 06:32:43 +0200

    Collateral files

--- python-packages/model_api/src/model_api/api.py
+++ python-packages/model_api/src/model_api/api.py
@@ -15,6 +15,8 @@ from typing import Dict
 import time
 import jwt
 from passlib.context import CryptContext
+import xgboost
+
 
 # JWT settings
 JWT_SECRET = "secret"
@@ -24,22 +26,22 @@ JWT_ALGORITHM = "HS256"
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 
-pwd_context = CryptContext(schemes=["bcrypt"], bcrypt__default_rounds=12, deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"], bcrypt__default_rounds=12, deprecated="auto"
+)
 
 # In-memory users database
 users_db = {
-    "testuser": {
-        "username": "testuser",
-        "password": pwd_context.hash("testpassword")
-    }
+    "testuser": {"username": "testuser", "password": pwd_context.hash("testpassword")}
 }
+
 
 def load_model(path: Optional[Path] = None):
     """Loads a saved ML model.
 
     The path to load the model is either provided by
         the env variable (`MODEL_PATH`). If `MODEL_PATH`
-        has not been set the `path` argument will be used. 
+        has not been set the `path` argument will be used.
 
     Raises:
         RuntimeError: If both the env variable `MODEL_PATH`
@@ -58,79 +60,91 @@ def load_model(path: Optional[Path] = None):
         print("No path to load model provided!")
         raise RuntimeError("No path to load model provided!")
 
-    model_path = os_model_path if os_model_path is not None else path 
+    model_path = os_model_path if os_model_path is not None else path
     # Loading the saved model - with a path for unittesting
     return joblib.load(model_path)
+
 
 loaded_model = load_model(Path("trained_model.joblib"))
 
 # Loading the saved model
 # loaded_model = joblib.load("src/models/trained_model.joblib")
 
+
 # Pydantic model for user schema
 class UserSchema(BaseModel):
     username: str
     password: str
 
-# Pydantic model for the prediction 
+
+# Pydantic model for the prediction
 class PredictRequestModel(BaseModel):
     features: Dict[str, float]
-    
+
+
 class ModelInputFeatures(BaseModel):
     place: int
-    catu: conint(ge=1, le=4)    #User category
-    sexe : conint(ge=1, le=2)   #sex is male (1) or female(2)
-    secu1 : float
-    year_acc : int
-    victim_age : int
-    catv : conint(ge=1, le=13)  #Category of vehicle
-    obsm : int 
-    motor : int
-    catr: int = Field(...) #Category of road 1-6 and 9!
-    
-    @field_validator('catr')
+    catu: conint(ge=1, le=4)  # User category
+    sexe: conint(ge=1, le=2)  # sex is male (1) or female(2)
+    secu1: float
+    year_acc: int
+    victim_age: int
+    catv: conint(ge=1, le=13)  # Category of vehicle
+    obsm: int
+    motor: int
+    catr: int = Field(...)  # Category of road 1-6 and 9!
+
+    @field_validator("catr")
     def validate_catr(cls, value):
         if value not in list(range(1, 7)) + [9]:
-            raise ValueError('catr must be between 1-6 or 9')
+            raise ValueError("catr must be between 1-6 or 9")
         return value
-      
-    circ : conint(ge=1, le=4)   #Traffic regime
-    surf : conint(ge=1, le=9)   #surface condition
-    situ : conint(ge=1, le=5)   #Situation of the accident
-    vma : int
-    jour : conint(ge=1, le=31)  # day should be between 1 and 31
-    mois : conint(ge=1, le=12)  # month should be between 1 and 12
-    lum : conint(ge=1, le=5)    # lighting conditions 5 categories
-    dep : int #Department
-    com : int #commune number
-    agg_ : int
-    int : conint(ge=1, le=9)    #type of intersection 9 categories
-    atm : conint(ge=1, le=9)    #Atmospheric conditions 9 categories
-    col :conint(ge=1, le=7)     #Type of collision 7 cat
-    lat : confloat(ge=-90, le=90)  # lat should be between -90 and 90
-    long : confloat(ge=-180, le=180)  # long should be between -180 and 180
-    hour : conint(ge=0, le=24)  #hour of the day
-    nb_victim : int
-    nb_vehicules : int 
- 
-#hashes a string password using bcrypt   
+
+    circ: conint(ge=1, le=4)  # Traffic regime
+    surf: conint(ge=1, le=9)  # surface condition
+    situ: conint(ge=1, le=5)  # Situation of the accident
+    vma: int
+    jour: conint(ge=1, le=31)  # day should be between 1 and 31
+    mois: conint(ge=1, le=12)  # month should be between 1 and 12
+    lum: conint(ge=1, le=5)  # lighting conditions 5 categories
+    dep: int  # Department
+    com: int  # commune number
+    agg_: int
+    int: conint(ge=1, le=9)  # type of intersection 9 categories
+    atm: conint(ge=1, le=9)  # Atmospheric conditions 9 categories
+    col: conint(ge=1, le=7)  # Type of collision 7 cat
+    lat: confloat(ge=-90, le=90)  # lat should be between -90 and 90
+    long: confloat(ge=-180, le=180)  # long should be between -180 and 180
+    hour: conint(ge=0, le=24)  # hour of the day
+    nb_victim: int
+    nb_vehicules: int
+
+
+# hashes a string password using bcrypt
 def hash_password(password: str):
     return pwd_context.hash(password)
 
-#verifies if string password matches the hashed password
+
+# verifies if string password matches the hashed password
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-#his function constructs a JSON response with an access token
+
+# his function constructs a JSON response with an access token
 def token_response(token: str):
     return {"access_token": token}
 
-#signing a JWT token using the provided payload and secret key
-#the token expires after 10 minutes
+
+# signing a JWT token using the provided payload and secret key
+# the token expires after 10 minutes
 def sign_jwt(user_id: str):
-    payload = {"user_id": user_id, "expires": time.time() + 600} # Token expires within 10 minutes
+    payload = {
+        "user_id": user_id,
+        "expires": time.time() + 6000,
+    }  # Token expires within 10 minutes
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
     return token_response(token)
+
 
 # this function decodes a JWT token to retrieve the payload
 # if the 10 minutes have not yet expired
@@ -140,28 +154,35 @@ def decode_jwt(token: str):
         return decoded_token if decoded_token["expires"] >= time.time() else None
     except Exception:
         return {}
-   
+
+
 def check_user(data: UserSchema):
     for user in users_db:
         if user.username == data.username and user.password == data.password:
             return True
     return False
 
+
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
         super(JWTBearer, self).__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
-        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        credentials: HTTPAuthorizationCredentials = await super(
+            JWTBearer, self
+        ).__call__(request)
         if credentials:
             if not credentials.scheme == "Bearer":
-                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+                raise HTTPException(
+                    status_code=403, detail="Invalid authentication scheme."
+                )
             if not self.verify_jwt(credentials.credentials):
-                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
+                raise HTTPException(
+                    status_code=403, detail="Invalid token or expired token."
+                )
             return credentials.credentials
         else:
             raise HTTPException(status_code=403, detail="Invalid authorization code.")
-
 
     def verify_jwt(self, jwtoken: str):
         isTokenValid: bool = False
@@ -173,8 +194,10 @@ class JWTBearer(HTTPBearer):
         if payload:
             isTokenValid = True
         return isTokenValid
-    
+
+
 api = FastAPI()
+
 
 @api.get("/", tags=["root"])
 async def read_root():
@@ -186,7 +209,7 @@ async def read_root_secured():
     return {"message": "Hello World! but secured"}
 
 
-#endpoint for user signup that registers new users and checks if user already exists
+# endpoint for user signup that registers new users and checks if user already exists
 @api.post("/user/signup", tags=["user"])
 async def create_user(user: UserSchema = Body(...)):
     if user.username in users_db:
@@ -195,27 +218,34 @@ async def create_user(user: UserSchema = Body(...)):
     users_db[user.username] = {"username": user.username, "password": hashed_password}
     return sign_jwt(user.username)
 
-#endpoint for user login that authenticates by checking if users are admin or signed up in the user database, 
+
+# endpoint for user login that authenticates by checking if users are admin or signed up in the user database,
 # veryfiys username and password and returns a JWT token
 @api.post("/user/login", tags=["user"])
 async def user_login(user: UserSchema = Body(...)):
     if ADMIN_USERNAME is None or ADMIN_PASSWORD is None:
-        raise HTTPException(status_code=500,
-                            detail=f"The OS env. variables `ADMIN_USERNAME` and `ADMIN_PASSWORD` have not been set!")
+        raise HTTPException(
+            status_code=500,
+            detail=f"The OS env. variables `ADMIN_USERNAME` and `ADMIN_PASSWORD` have not been set!",
+        )
 
-    if user.username in users_db and verify_password(user.password, users_db[user.username]["password"]):
+    if user.username in users_db and verify_password(
+        user.password, users_db[user.username]["password"]
+    ):
         return sign_jwt(user.username)
     elif user.username == ADMIN_USERNAME and user.password == ADMIN_PASSWORD:
         return sign_jwt(user.username)
     raise HTTPException(status_code=401, detail="Invalid username or password")
 
-#endpoint to predict with the loaded model
+
+# endpoint to predict with the loaded model
 @api.post("/predict", dependencies=[Depends(JWTBearer())], tags=["prediction"])
 def predict_model(features: ModelInputFeatures):
     input_df = pd.DataFrame([features.model_dump()])
     print(input_df)
     prediction = loaded_model.predict(input_df)
     return {"prediction": prediction.tolist()}
+
 
 def get_feature_values_manually(feature_names):
     features = {}
@@ -224,18 +254,20 @@ def get_feature_values_manually(feature_names):
         features[feature_name] = feature_value
     return features
 
+
 @api.get("/refresh", dependencies=[Depends(JWTBearer())], tags=["refresh"])
 def refresh_model():
     print("refresh!")
-    #function will be using and modifying the global variable loaded_model, because it was defined outside this function
+    # function will be using and modifying the global variable loaded_model, because it was defined outside this function
     global loaded_model
     # Reload the model
     loaded_model = load_model()
     return {"message": "The Model was updated!"}
 
+
 if __name__ == "__main__":
-    
-     # if len(sys.argv) == 2:
+
+    # if len(sys.argv) == 2:
     #     json_file = sys.argv[1]
     #     with open(json_file, 'r') as file:
     #         features = json.load(file)
@@ -247,5 +279,5 @@ if __name__ == "__main__":
     # model_input = ModelInputFeatures(**features)
     # result = predict_model(model_input)
     # print(f"prediction : {result['prediction'][0]}")
-    
+
     uvicorn.run(api, host="0.0.0.0", port=8000)
