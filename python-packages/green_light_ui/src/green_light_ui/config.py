020ff7d2daa85962322cf1d76b4cb68cfccb1fa7
probinb <paula.r.beck@gmail.com>
2024-07-08 14:17:52 +0200

    added evans and Paulas LinkedIn profile and Github

--- python-packages/green_light_ui/src/green_light_ui/config.py
+++ python-packages/green_light_ui/src/green_light_ui/config.py
@@ -10,8 +10,15 @@ from member import Member
 TITLE = "MLOPS Bootcamp - May 24"
 
 TEAM_MEMBERS = [
-    Member("Paula"),
-    Member("Evan"),
+    Member(
+        name="Paula",
+        linkedin_url="https://www.linkedin.com/in/paularbeck/",
+        github_url="https://github.com/probinb"
+        ),
+    Member(name="Evan",
+           linkedin_url="https://www.linkedin.com/in/stromatias/",
+           github_url="https://github.com/evan-stromatias"
+           ),
     Member(
         name="Josef",
         linkedin_url="https://www.linkedin.com/in/dr-josef-hartmann-3663935/",
