969a3b52c898b59bfad23b974c16434f7962cebf
probinb <paula.r.beck@gmail.com>
2024-07-08 22:36:58 +0200

    edited file paths to dockerize

--- python-packages/model_api/src/model_api/api.py
+++ python-packages/model_api/src/model_api/api.py
@@ -65,10 +65,10 @@ def load_model(path: Optional[Path] = None):
     return joblib.load(model_path)
 
 
-# loaded_model = load_model(Path("trained_model.joblib"))
+loaded_model = load_model(Path("trained_model.joblib"))
 
-# Loading the saved model
-loaded_model = joblib.load("Volumes/models/trained_model.joblib")
+# Loading the saved model locally
+# loaded_model = joblib.load("Volumes/models/trained_model.joblib")
 
 # Pydantic model for user schema
 class UserSchema(BaseModel):
@@ -260,7 +260,7 @@ def refresh_model():
     # function will be using and modifying the global variable loaded_model, because it was defined outside this function
     global loaded_model
     # Reload the model
-    loaded_model = load_model("Volumes/models/trained_model.joblib")
+    loaded_model = load_model()
     return {"message": "The Model was updated!"}
 
 
