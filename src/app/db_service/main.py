d4bd1d1ddf7d8ccef311c425a6d24cbfc45df965
Soumaya-JE <140071440+Soumaya-JE@users.noreply.github.com>
2024-09-04 19:48:21 +0200

    Add files via upload

---
+++ src/app/db_service/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from passlib.context import CryptContext
+
+app = FastAPI()
+
+security = HTTPBasic()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+users = {
+    "user1": {
+        "username": "user1",
+        "name": "Sousou",
+        "hashed_password": pwd_context.hash('datascientest'),
+        "role": "standard",
+    },
+    "user2": {
+        "username": "user2",
+        "name": "Mim",
+        "hashed_password": pwd_context.hash('secret'),
+        "role": "standard",
+    },
+    "admin": {
+        "username": "admin",
+        "name": "Admin",
+        "hashed_password": pwd_context.hash('adminsecret'),
+        "role": "admin",
+    }
+}
+
+def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    username = credentials.username
+    user = users.get(username)
+    if not user or not pwd_context.verify(credentials.password, user['hashed_password']):
+        raise HTTPException(
+            status_code=401,
+            detail="Identifiant ou mot de passe incorrect",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return user
+
+@app.get("/query")
+def query_db(user: dict = Depends(get_current_user)):
+    if user['role'] != 'admin':
+        raise HTTPException(
+            status_code=403,
+            detail="Droits non autorisés",
+        )
+    return {"message": "Bienvenue sur notre base de données"}
+
