1a3e12bcb646841711f6ea98da5dead93563dcae
Ubuntu <ubuntu@ip-172-31-19-93.eu-west-1.compute.internal>
2024-10-09 12:39:23 +0000

    Trained and evaluated a Random Forest Classifier with n_estimators = 200 and criterion = entropy

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -14,7 +14,7 @@ y_test = pd.read_csv('data/preprocessed/y_test.csv')
 y_train = np.ravel(y_train)
 y_test = np.ravel(y_test)
 
-rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1)
+rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1,n_estimators = 200, criterion = "entropy")
 
 #--Train the model
 rf_classifier.fit(X_train, y_train)
