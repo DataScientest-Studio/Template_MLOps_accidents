6186c155e34c9972dc60a3498acd63a51f97992e
Ubuntu <ubuntu@ip-172-31-24-214.eu-west-1.compute.internal>
2025-04-19 12:10:24 +0000

    Trained and evaluated a first Random Forest Classifier

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -55,7 +55,7 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     df = merge_datasets(df_users, df_veh, df_places, df_caract)
 
     # Add new columns
-    df = add_new_columns(df, nb_victim, nb_vehicules)
+    #df = add_new_columns(df, nb_victim, nb_vehicules)
 
     # Modify target variable
     df = modif_target_variable(df)
